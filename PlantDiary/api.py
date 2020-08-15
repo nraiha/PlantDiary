84eee9d581121a12ddfb62489659ccac9a86cb37
Linux User <nuutti@hacked.localdomain>
2020-08-15 16:29:15 +0300

    Hello World

---
+++ PlantDiary/api.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+from flask_restful import Api
+
+from .resources.diary import
+from .resources.plant_item import
+from .resources.plant_general import 
---
+++ PlantDiary/constants.py
@@ -0,0 +1,6 @@
+MASON = "application/vnd.mason+json"
+LINK_RELATIONS_URL = "/PD/link-relations/"
+ERROR_PROFILE = "/profiles/error/"
+DIARY_PROFILE = "/profiles/diary/"
+PLANT_ITEM_PROFILE = "/profiles/plant_item/"
+PLANT_GENERAL_PROFILE = "/profiles/plant_general/"
